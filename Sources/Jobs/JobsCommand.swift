2854382e337482619a45a7ba75f9ef1b80bb6ee9
{rnantes} <{reid.nantes@gmail.com}>
2019-01-26 12:07:29 -0500

    one event loop now dedicated to scheduling

--- Sources/Jobs/JobsCommand.swift
+++ Sources/Jobs/JobsCommand.swift
@@ -4,17 +4,19 @@ import NIO
 
 /// The command to start the Queue job
 public class JobsCommand: Command {
-    
+
+    let jobScheduler = JobScheduler()
+
     /// See `Command`.`arguments`
     public var arguments: [CommandArgument] = []
-    
+
     /// See `Command`.`options`
     public var options: [CommandOption] {
         return [
             CommandOption.value(name: "queue")
         ]
     }
-    
+
     private var isShuttingDown: Bool {
         get {
             self._lock.lock()
@@ -30,22 +32,23 @@ public class JobsCommand: Command {
 
     private var _isShuttingDown: Bool = false
     private var _lock: NSLock
-    
+
     /// See `Command`.`help`
-    public var help: [String] = ["Runs queued worker jobs"]
-    
+    public var help: [String] = ["Rugetgetns queued worker jobs"]
+
     /// Creates a new `JobCommand`
     public init() {
         _lock = NSLock()
     }
-    
+
     /// See `Command`.`run(using:)`
     public func run(using context: CommandContext) throws -> EventLoopFuture<Void> {
-        context.console.info("Starting Jobs worker")
+        print("HERE1")
+        print("System.coreCount: \(System.coreCount)")
 
         let elg = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
         let signalQueue = DispatchQueue(label: "vapor.jobs.command.SignalHandlingQueue")
-        
+
         //SIGTERM
         let termSignalSource = DispatchSource.makeSignalSource(signal: SIGTERM, queue: signalQueue)
         termSignalSource.setEventHandler {
@@ -55,7 +58,7 @@ public class JobsCommand: Command {
         }
         signal(SIGTERM, SIG_IGN)
         termSignalSource.resume()
-        
+
         //SIGINT
         let intSignalSource = DispatchSource.makeSignalSource(signal: SIGINT, queue: signalQueue)
         intSignalSource.setEventHandler {
@@ -65,31 +68,51 @@ public class JobsCommand: Command {
         }
         signal(SIGINT, SIG_IGN)
         intSignalSource.resume()
-        
+
+        print("here 2")
+
         var shutdownPromises: [EventLoopPromise<Void>] = []
+
+        var i = 0
         for eventLoop in elg.makeIterator()! {
             let sub = context.container.subContainer(on: eventLoop)
             let console = context.console
             let queueName = context.options["queue"] ?? QueueType.default.name
             let shutdownPromise: EventLoopPromise<Void> = eventLoop.newPromise()
-            
             shutdownPromises.append(shutdownPromise)
-            
-            eventLoop.submit {
-                try self.setupTask(eventLoop: eventLoop,
-                                   container: sub,
-                                   queueName: queueName,
-                                   console: console,
-                                   promise: shutdownPromise)
-            }.catch {
-                console.error("Could not boot EventLoop: \($0)")
+
+            if i == 0 {
+                eventLoop.submit {
+                    try self.jobScheduler.scheduleTask(eventLoop: eventLoop,
+                                                       container: sub,
+                                                       console: console)
+                    }.catch {
+                        console.error("Could not boot EventLoop: \($0)")
+                }
+            } else {
+                eventLoop.submit {
+                    try self.setupTask(eventLoopID: i,
+                                       eventLoop: eventLoop,
+                                       container: sub,
+                                       queueName: queueName,
+                                       console: console,
+                                       promise: shutdownPromise)
+                    }.catch {
+                        console.error("Could not boot EventLoop: \($0)")
+                }
             }
+            i += 1
         }
-        
+
+        print("here 3")
+
         return .andAll(shutdownPromises.map { $0.futureResult }, eventLoop: elg.next())
     }
-    
-    private func setupTask(eventLoop: EventLoop,
+
+
+    // sets up
+    private func setupTask(eventLoopID: Int,
+                           eventLoop: EventLoop,
                            container: SubContainer,
                            queueName: String,
                            console: Console,
@@ -100,38 +123,46 @@ public class JobsCommand: Command {
         let jobContext = (try? container.make(JobContext.self)) ?? JobContext()
         let key = queue.makeKey(with: queueService.persistenceKey)
         let config = try container.make(JobsConfig.self)
-        
+
+        print("setupTask - \(eventLoopID)")
+
         _ = eventLoop.scheduleRepeatedTask(initialDelay: .seconds(0), delay: queueService.refreshInterval) { task -> EventLoopFuture<Void> in
             return queueService.persistenceLayer.get(key: key, jobsConfig: config).flatMap { jobData in
+
                 //Check if shutting down
-                
                 if self.isShuttingDown {
                     task.cancel()
                     promise.succeed()
                 }
-                
+
                 //No job found, go to the next iteration
-                guard let jobData = jobData else { return eventLoop.future() }
+                guard let jobData = jobData else {
+                    print("NO Job: \(eventLoopID)")
+                    return eventLoop.future()
+                }
+                print("Job FOUND: \(eventLoopID)")
+
+
+                // job is found, dequeue
                 let job = jobData.data
-                console.info("Dequeing Job job_id=[\(jobData.id)]", newLine: true)
-                
+                console.info("Dequeing Job \(jobData.id)", newLine: true)
+
                 let futureJob = job.dequeue(context: jobContext, worker: eventLoop)
                 return self.firstFutureToSucceed(future: futureJob, tries: jobData.maxRetryCount, on: eventLoop)
                     .flatMap { _ in
                         guard let jobString = job.stringValue(key: key, maxRetryCount: jobData.maxRetryCount, id: jobData.id) else {
-                            console.error("Error: Could not get string value of Job. job_id=[\(jobData.id)]", newLine: true)
                             return eventLoop.future(error: Abort(.internalServerError))
                         }
-                        
+
                         return queueService.persistenceLayer.completed(key: key, jobString: jobString)
                     }
                     .catchFlatMap { error in
-                        console.error("Error: \(error) job_id=[\(jobData.id)]", newLine: true)
-                        
+                        console.error("[\(jobData.id)] Error: \(error)", newLine: true)
+
                         guard let jobString = job.stringValue(key: key, maxRetryCount: jobData.maxRetryCount, id: jobData.id) else {
                             return eventLoop.future(error: Abort(.internalServerError))
                         }
-                        
+
                         return queueService
                             .persistenceLayer
                             .completed(key: key, jobString: jobString)
@@ -142,7 +173,7 @@ public class JobsCommand: Command {
             }
         }
     }
-    
+
     /// Returns the first time a given future succeeds and is under the `tries`
     ///
     /// - Parameters:
@@ -153,12 +184,12 @@ public class JobsCommand: Command {
     private func firstFutureToSucceed<T>(future: Future<T>, tries: Int, on worker: EventLoopGroup) -> Future<T> {
         return future.map { complete in
             return complete
-        }.catchFlatMap { error in
-            if tries == 0 {
-                return worker.future(error: error)
-            } else {
-                return self.firstFutureToSucceed(future: future, tries: tries - 1, on: worker)
-            }
+            }.catchFlatMap { error in
+                if tries == 0 {
+                    return worker.future(error: error)
+                } else {
+                    return self.firstFutureToSucceed(future: future, tries: tries - 1, on: worker)
+                }
         }
     }
 }
